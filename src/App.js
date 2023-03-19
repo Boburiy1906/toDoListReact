a1187165d6e7317e72f814975d11e4f7acfdbf19
Boburiy1906 <Boburshoh1906@gmail.com>
2023-03-19 21:56:53 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+function App() {
+    return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./Form.css";
+
+function Form({ setData }) {
+    return <form></form>;
+}
+
+export default Form;
